54d0bbf4cfeb92a08e723c1dd5de6526f1961345
Patryk Gneba <gnebapatryk@gmail.com>
2022-10-22 20:26:58 +0200

    Implement Automapper + mappings

---
+++ HR.LeaveManagment.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HR.LeaveManagment.Application.DTOs;
+using HR.LeaveManagment.Application.DTOs.LeaveRequest;
+using HR.LeaveManagment.Domain.Entities;
+
+namespace HR.LeaveManagment.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+              CreateMap<LeaveRequest,LeaveRequestDto>().ReverseMap();
+              CreateMap<LeaveRequest,LeaveRequestListDto>().ReverseMap();
+              CreateMap<LeaveType,LeaveTypeDto>().ReverseMap();
+              CreateMap<LeaveAllocation,LeaveAllocationDto>().ReverseMap();
+        }
+    }
+}
